75b4c6920be6c8b0b7b110e60eda77dad68241f2
poisun <poma.komarow@gmail.com>
2022-02-23 01:06:42 +0300

    fix: fix deploy error

--- src/bot/middlewares/setup-session.middleware.ts
+++ src/bot/middlewares/setup-session.middleware.ts
@@ -1,11 +1,6 @@
 import { session } from 'grammy';
 import { FileAdapter } from '@satont/grammy-file-storage';
 import { initialSessionData } from '../types/session';
-import { MyContext } from '../types/context';
-
-function getSessionKey(ctx: MyContext): string | undefined {
-  return ctx.from?.id.toString();
-}
 
 export const middleware = () =>
   session({
@@ -13,5 +8,4 @@ export const middleware = () =>
     storage: new FileAdapter({
       dirName: 'filedb',
     }),
-    getSessionKey,
   });
